9b45e50d18f9913b270b6b9fb7e242932530c86f
Brad Cole <artwired@users.noreply.github.com>
2023-06-26 22:19:02 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,53 +1,40 @@
+let homeScoreEl = document.getElementById("home-score");
+let guestScoreEl = document.getElementById("guest-score");
 
-let homeScoreEl = document.getElementById("home-score")
-let guestScoreEl = document.getElementById("guest-score")
+let homeScore = 0;
+let guestScore = 0;
 
-let homeScore = 0
-let guestScore = 0
-
+// Keeps track and highlights the leader
 function theLeader() {
-    if(homeScore > guestScore) {
-        homeScoreEl.classList.add("leader")
-        guestScoreEl.classList.remove("leader")
-    } else if(homeScore < guestScore){
-        guestScoreEl.classList.add("leader")
-        homeScoreEl.classList.remove("leader")
-    } else {
-        homeScoreEl.classList.remove("leader")
-        guestScoreEl.classList.remove("leader")
-    }
+  if (homeScore > guestScore) {
+    homeScoreEl.classList.add("leader");
+    guestScoreEl.classList.remove("leader");
+  } else if (homeScore < guestScore) {
+    guestScoreEl.classList.add("leader");
+    homeScoreEl.classList.remove("leader");
+  } else {
+    homeScoreEl.classList.remove("leader");
+    guestScoreEl.classList.remove("leader");
+  }
 }
 
 // Home score board functionality
-function homeAddOne() {
-    homeScore += 1
-    homeScoreEl.textContent = homeScore
-    theLeader()
-}
-function homeAddTwo() {
-    homeScore += 2
-    homeScoreEl.textContent = homeScore
-    theLeader()
-}
-function homeAddThree() {
-    homeScore += 3
-    homeScoreEl.textContent = homeScore
-    theLeader()
+function add(name, points) {
+  if (name === "home") {
+    homeScore += points;
+    homeScoreEl.textContent = homeScore;
+  } else if (name === "guest") {
+    guestScore += points;
+    guestScoreEl.textContent = guestScore;
+  }
+  theLeader();
 }
 
-// Guest score board functionality
-function guestAddOne() {
-     guestScore += 1
-     guestScoreEl.textContent = guestScore
-     theLeader()
-}
-function guestAddTwo() {
-    guestScore += 2
-    guestScoreEl.textContent = guestScore
-    theLeader()
-}
-function guestAddThree() {
-    guestScore += 3
-    guestScoreEl.textContent = guestScore
-    theLeader()
+function reset() {
+  homeScore = 0;
+  homeScoreEl.textContent = homeScore;
+  guestScore = 0;
+  guestScoreEl.textContent = guestScore;
+  homeScoreEl.classList.remove("leader");
+  guestScoreEl.classList.remove("leader");
 }
